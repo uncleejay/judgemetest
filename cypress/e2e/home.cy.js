3a48425265c318833b9c3f3050065443fb29a8a3
Uncleejay <joseph.c.o216@gmail.com>
2024-12-10 16:59:24 +0000

    Started Implementing test cases for judge.me site

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,53 +1,16 @@
-import Home_PO from "../support/pageObjects/Home_PO";
-import Search_PO from "../support/pageObjects/Search_PO";
-import 'cypress-axe'
+describe('Judge.Me Site', () => {
 
-describe('Judge.Me Site E2E Test', () => {
-
-    const baseUrl = 'https://judge.me/reviews';
-    const home_po = new Home_PO();
-    const search_po = new Search_PO();
+    const baseUrl = 'https://judge.me/reviews'
 
     beforeEach(() => {
+      // visit the judge.me site before each test
       cy.visit(baseUrl);
-      cy.injectAxe();
     });
 
-    // Verify Page Load
+    // Test Case 1: Verify Page Load
     it('should load the page successfully and display key elements', () => {
-        home_po.verifyPageLoad(baseUrl)
+        //check if the url is correct
+        cy.url().should('eq', baseUrl)
     });
 
-    // Validate Search Functionality
-    it('should display correct results when searching by keyword', () => {
-        search_po.verifySearchFunctionality();
-    });
-
-    // Validate Search Handles result when keyword is not found
-    it('should display result not found when searching for a non existent keyword', () => {
-        search_po.validateSearchNotFoundErr();
-    });
-
-    // Validate Pagination
-    it('should allow navigation through pagination controls', () => {
-        search_po.validatePagination()
-    });
-
-    // Validate Filtering by Rating
-    it('should validate filter by rating a 2.5', () => {
-        search_po.validateRateFilter()
-    });
-
-    // Validate currency filter
-    it('should validate currency filter to select EUR', () => {
-        search_po.validateCurrencyFilter()
-    })
-
-    it('should have no accessibility violations', () => {
-        cy.checkA11y();
-    });
-
-    it('should run lighthouse performance audit', () => {
-        home_po.verifyLighthousePerformance()
-    });
 });
